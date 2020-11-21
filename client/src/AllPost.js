33163e17ff10e499951fba9cb6e9ead398bbf41d
shaxqq <39559342+shaxqq@users.noreply.github.com>
2020-11-21 16:48:11 +0200

    change tab posts

--- client/src/AllPost.js
+++ client/src/AllPost.js
@@ -4,32 +4,74 @@ import {
   Card,
   CardContent,
   CardActions,
-  Fab, TextField, Container, Button, Box, Select, MenuItem, Tabs, Tab, FormControl, InputLabel,  
+  Fab,
+  TextField,
+  Container,
+  Button,
+  Box,
+  Select,
+  MenuItem,
+  Tabs,
+  Tab,
+  FormControl,
+  InputLabel,
+  Snackbar,
 } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
- // import AddIcon from "@material-ui/icons/Add";
+// import AddIcon from "@material-ui/icons/Add";
 import apis from "./api/api";
 import { useStyles } from "./styles";
 
+const Alert = (props) => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+};
+
+const TabPanel = ({ children, value, index }) => {
+  return <div>{value === index && <div>{children}</div>}</div>;
+};
+
+const groupBy = (xs, fn) => {
+  return xs.reduce(function (rv, x) {
+    (rv[fn(x)] = rv[fn(x)] || []).push(x);
+    return rv;
+  }, {});
+};
+
 export const AllPost = () => {
   const classes = useStyles();
-  const [text, setText] = useState("");
+  const [posts, setPosts] = useState([]);
   const [post, setPost] = useState({
     title: "",
     city: "",
     content: [],
   });
-  const [tab, setTab] = useState(0);
+  const [tab, setTab] = useState("0");
+  const [open, setOpen] = useState(false);
+  const [colorTab, setColorTab] = useState("");
 
-
+  // const badgeVisible = () => {
+  //   if (text.length === text.length) {
+  //     console.log("on");
+  //
+  //     setColorTab(!colorTab);
+  //   }
+  //   console.log("off");
+  // };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
   const handleChange = (event, newTab) => {
     setTab(newTab);
   };
   const changeTitle = (e) => {
     e.persist();
     setPost((param) => {
-      console.log(param)
+      console.log(param);
       return {
         ...param,
         title: e.target.value,
@@ -39,13 +81,12 @@ export const AllPost = () => {
   const changeName = (e) => {
     e.preventDefault();
     setPost((param) => {
-      console.log(param)
+      console.log(param);
       return {
         ...param,
         city: e.target.value,
       };
     });
-
   };
   const changeContent = (e) => {
     e.persist();
@@ -58,135 +99,66 @@ export const AllPost = () => {
   };
   const handlePost = async () => {
     await apis.createPost(post).then(() => {
-      if(post.content === ''){
+      if (post.content === "") {
         window.alert("Нужно заполнить все поля");
-        return post
+        return post;
       }
-      window.alert("Post add success");
+      setOpen(true);
       setPost(post);
-      window.location.href = "/";
-    //  console.log(post.content);
+      window.setTimeout(() => {
+        window.location.href = "/";
+      }, 1500);
+      //  console.log(post.content);
+    });
+  };
+  const updatePost = async () => {
+    await apis.updatePostById(post._id, post).then(() => {
+      //  console.log(child._id, child);
+      // window.location.href = `/post/${child._id}`
     });
   };
 
-  const TabPanel = () => {
- 
-    for (let i = 0; i < text.length; i++) {
-      const element = text[i].props.children[0].props.children[1].props.children;
-    //  console.log(value)
-    //  console.log(index)
-     
-      if(element === `${tab}`) {
-        console.log(text[i])
-      return( <div>
-          <Box p={3}>
-            {text[i]}
-          </Box>
-      </div>)
-    } 
-  }
-    
-  }
-
-  console.log(TabPanel())
-
- 
-  
+  // console.log(TabPanel())
 
   useEffect(() => {
     apis.getAllPost().then((post) => {
-        
-      const dataPost = post.data.data.map((child) => {
-        
-        const deletePost = (e) => {
-          e.preventDefault();
-          if (window.confirm(`Удалить данный пост ${child._id} ?`)) {
-            apis.deletePostById(child._id);
-            window.location.reload();
-          }
-        };
-        const updatePost = async () => {
-          await apis.updatePostById(child._id, child).then(() => {
-    //  console.log(child._id, child);
-
-            // window.location.href = `/post/${child._id}`
-          });
-        };
-
-      //  console.log(child.city);
-        return (
-          <Card key={child._id} className={classes.allCardPost}>
-            <CardContent>
-              <Typography >
-                {child.title}
-              </Typography>
-              <Typography color="textSecondary" className={classes.allUserPost}>
-                {child.city}
-              </Typography>
-              <Typography >
-                {child.content}
-              </Typography>
-            </CardContent>
-            <CardActions className={classes.allFooterCardPost}>
-              <Fab
-                color="primary"
-                aria-label="edit"
-                size="small"
-                onClick={updatePost}
-              >
-                <EditIcon />
-              </Fab>
-
-              <Fab
-                color="secondary"
-                aria-label="delete"
-                size="small"
-                onClick={deletePost}
-              >
-                <DeleteIcon />
-              </Fab>
-            </CardActions>
-          </Card>
-        );
-      });
-      return setText(dataPost);
+      setPosts(post.data.data);
     });
   }, []);
- 
- //console.log(text)
 
+  //const forms = text.map((child)=>{console.log(child)})
+  const postsByCity = groupBy(posts, (post) => post.city);
+  //console.log()
 
- 
- 
-
- //const forms = text.map((child)=>{console.log(child)})
+ // console.log(postsId);
+ // console.log(posts);
+ // console.log(postsByCity);
 
   return (
     <div>
-       <Container className={classes.createRoot}>
+      <Container className={classes.createRoot}>
         <Box className={classes.createInputBox}>
           <TextField id="title" label="Title" onChange={changeTitle} />
-          <FormControl className={classes.formControl} >
-        <InputLabel id="demo-simple-select-label">City </InputLabel>
-        <Select
-          labelId="city"
-          id="city"
-          value={post.city}
-          onChange={changeName}
-          style={{paddingRight: '1px'}}
-          >
-          <MenuItem value={`0`}>option-0</MenuItem>
-          <MenuItem value={`1`}>option-1</MenuItem>
-          <MenuItem value={`2`}>option-2</MenuItem>
-          <MenuItem value={`3`}>option-3</MenuItem>
-          </Select>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">City </InputLabel>
+            <Select
+              labelId="city"
+              id="city"
+              value={post.city}
+              onChange={changeName}
+              style={{ paddingRight: "1px" }}
+            >
+              <MenuItem value={`0`}>option-0</MenuItem>
+              <MenuItem value={`1`}>option-1</MenuItem>
+              <MenuItem value={`2`}>option-2</MenuItem>
+              <MenuItem value={`3`}>option-3</MenuItem>
+            </Select>
           </FormControl>
-          
         </Box>
-                <TextField
+        <TextField
           id="content"
           label="Text"
-          rows={3}
+          rows={6}
           multiline
           rowsMax={50}
           className={classes.createInputField}
@@ -208,39 +180,71 @@ export const AllPost = () => {
       </Container>
 
       <div className={classes.postsTable}>
-        <Tabs  orientation="vertical"
-        variant="scrollable"
-        value={tab}
-        onChange={handleChange}
-        aria-label="Vertical tabs example">
-           <Tab label="Item One" index={0} />
-        <Tab label="Item Two"  index={1} />
-        <Tab label="Item Three" index={2}/>
-        <Tab label="Item four" index={3}/>
-           </Tabs>
-        
-          { text ? <TabPanel /> : null }
-          { text ? <TabPanel /> : null }
-       
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={tab}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          textColor="primary"
+        >
+          {Object.keys(postsByCity).map((city) => (
+            <Tab label={city} value={city} key={city} />
+          ))}
+        </Tabs>
+        {Object.keys(postsByCity).map((city) => (
+          <TabPanel value={tab} index={city} key={city}>
+            {postsByCity[city].map((post) => (
+              <Card key={post._id} className={classes.allCardPost}>
+                <CardContent>
+                  <Typography>{post.title}</Typography>
+                  <Typography
+                    color="textSecondary"
+                    className={classes.allUserPost}
+                  >
+                    {post.city}
+                  </Typography>
+                  <Typography>{post.content}</Typography>
+                </CardContent>
+                <CardActions className={classes.allFooterCardPost}>
+                  <Fab
+                    color="primary"
+                    aria-label="edit"
+                    size="small"
+                    onClick={updatePost}
+                  >
+                    <EditIcon />
+                  </Fab>
+
+                  <Fab
+                    color="secondary"
+                    aria-label="delete"
+                    size="small"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      if (window.confirm(`Удалить данный пост ${post._id} ?`)) {
+                        apis.deletePostById(post._id);
+                        window.location.reload();
+                      }
+                    }}
+                  >
+                    <DeleteIcon />
+                  </Fab>
+                </CardActions>
+              </Card>
+            ))}
+          </TabPanel>
+        ))}
       </div>
-     
+      <Snackbar open={open} autoHideDuration={1000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          Пост добавлен!
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
 //   <TextField id="username" label="nickName" onChange={changeName} />
-
-
-   
-//      <TabPanel value={0} index={0}>
-//                 
-//      </TabPanel>
-//      <TabPanel value={`1`} index={1}>
-//      
-//      </TabPanel>
-//      <TabPanel value={'2'} index={2}>
-//      
-//      </TabPanel>
-
 
 //  { text ? <TabPanel value={0} index={0} /> : null }
 //  { text ? <TabPanel value={1} index={1} /> : null }
