9c05cafdcee5bce03862ad576a972cd8a44bd07d
shaxqq <39559342+shaxqq@users.noreply.github.com>
2020-12-02 15:22:03 +0200

    db

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 
-// mongodb+srv://sazeke:sazekeL1@cluster0.nv88g.mongodb.net/informationSupport?retryWrites=true&w=majority
+// heroku config:set MONGODB_URI="mongodb+srv://sazeke:sazekeL1@cluster0.nv88g.mongodb.net/informationSupport?retryWrites=true&w=majority"
 
 mongoose
   .connect(process.env.MONGODB_URI || 
-    "mongodb+srv://sazeke:sazekeL1@cluster0.nv88g.mongodb.net/sample_mflix?retryWrites=true&w=majority",
+    "mongodb+srv://sazeke:sazekeL1@cluster0.nv88g.mongodb.net/informationSupport?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .catch((e) => {
