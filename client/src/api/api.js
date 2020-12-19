b333aa1ff0afeb85630411e0329e15b57ca94bf1
shaxqq <39559342+shaxqq@users.noreply.github.com>
2020-12-19 12:37:08 +0200

    log

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api'})
+const api = axios.create({ baseURL: 'http://localhost:8000/api' || '/api'})
 //http://localhost:8080/api
 export const createPost = param => api.post(`/post`, param)
 export const getAllPost = () => api.get(`/posts`)
