2ef5267f7d3f03e81be847694203cb93e8a7ddc2
Sobolev Roman <39559342+shaxqq@users.noreply.github.com>
2021-06-17 13:45:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
 
-app.get('/', (req, res) => {
-    res.send('message: server start!')
-})
+// app.get('/', (req, res) => {
+//    res.send('message: server start!')
+//})
+const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'src', 'App.js'));
+  });
 
 app.use('/api', router)
 
