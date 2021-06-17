1ae9e7c8d332f79f6bf5526d9feb091680956deb
Sobolev Roman <39559342+shaxqq@users.noreply.github.com>
2021-06-17 13:52:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production'){
 //})
 const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'src', 'App.js'));
+    res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.js'));
   });
 
 app.use('/api', router)
